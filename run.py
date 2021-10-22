c505e36059f89bacc9e3e7cf7407c32d0958d83b
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-10-23 00:17:04 +0300

    stated class methods

--- run.py
+++ run.py
@@ -4,3 +4,17 @@ import pyperclip
 class User:
     user_list = []
 
+    @classmethod
+    def show_user(cls):
+        return cls.user_list
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        User.user_list.append(self)
+
+    def delete_user(self):
+        User.user_list.remove(self)
+
