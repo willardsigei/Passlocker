a4f26180f4bfb6baeee2505d66261684439b8a77
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-10-23 00:28:52 +0300

    added first testclass on users_test.py

--- users_test.py
+++ users_test.py
@@ -1,3 +1,16 @@
 import unittest
 from users import User
 from users import Credentials
+
+class TestClass(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User('Willard','willard')
+
+    def test_init(self):
+        self.assertEqual(self.new_user.username,'Willard')
+        self.assertEqual(self.new_user.password,'willard')
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
