247fd726f496988c502c616486c014b050fcebad
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-10-23 00:22:38 +0300

    added @classmethods

--- run.py
+++ run.py
@@ -39,3 +39,21 @@ class Credentials():
 
     def delete_credentials(self):
         Credentials.credentials_list.remove(self)
+    
+    @classmethod
+    def find_credential(cls, account):
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return credential
+
+    @classmethod
+    def copy_password(cls,account):
+        found_credentials = Credentials.find_credential(account)
+        pyperclip.copy(found_credentials.password)
+
+    @classmethod
+    def if_credential_exist(cls, account):
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+        return False
