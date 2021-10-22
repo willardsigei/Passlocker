557596cb908eeac9ede2642a746277c186bcc21a
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-10-23 01:01:56 +0300

    login, create new, save and show user functions

--- run.py
+++ run.py
@@ -9,3 +9,15 @@ def save_user(user):
 
 def show_user():
     return User.show_user()
+
+def login_user(username,password):
+    check_user = Credentials.verify_user(username,password)
+    return check_user
+
+def create_new_credential(account,userName,password):
+    new_credential = Credentials(account,userName,password)
+    return new_credential
+def save_credentials(credentials):
+    credentials. save_details()
+def show_accounts_details():
+    return Credentials.show_credentials()
